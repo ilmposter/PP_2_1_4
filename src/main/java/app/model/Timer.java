08147373cc73bb3412f2e2edeffa1ec72938c9a2
ilmposter <97394212+ilmposter@users.noreply.github.com>
2024-07-09 00:15:35 +0300

    second commit pp2_1_4

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,9 +1,13 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component("timerBean")
 public class Timer {
 
     private Long nanoTime = System.nanoTime();
-
     public Long getTime() {
         return nanoTime;
     }
